4f72068ef1c0797afcb1b5e6708481ca2ffc4e7e
Matt Hooper <matt.j.hooper@googlemail.com>
2021-01-31 21:47:46 +0000

    Refactor IntegrationTests

--- MusicTutor.IntegrationTests/PupilsControllerTests.cs
+++ MusicTutor.IntegrationTests/PupilsControllerTests.cs
@@ -23,14 +23,43 @@ namespace MusicTutor.IntegrationTests
         [Fact]
         public async Task PupilCanBeCreatedUpdatedAndDeleted()
         {
+            var piano = await GetPiano();
+
+            var createPupilRequest = new CreatePupil("Pupil Name", 21M, DateTime.Today, 14, piano.Id, "Contact Name", "Contact Email", "Contact Phone Number");
+            var pupilResponse = await CreatePupilAndValidate(createPupilRequest);
+
+            await GetPupilAndValidate(pupilResponse);
+
+            var updatePupilRequest = new UpdatePupil(pupilResponse.Id, "New Name", pupilResponse.LessonRate + 1, pupilResponse.StartDate.AddDays(-1), 7, "New Contact Name", "New Contact Email", "New PhoneNo" );
+            pupilResponse = await UpdatePupilAndValidate(updatePupilRequest);
+
+            await DeletePupilAndValidate(pupilResponse.Id);
+        }
+
+        private async Task<InstrumentResponseDto> GetPiano()
+        {
             var response = await _client.GetAsync(InstrumentsUri);
             response.StatusCode.Should().Be(StatusCodes.Status200OK);
 
             var content = await response.Content.ReadAsAsync<List<InstrumentResponseDto>>();
             var piano = content.Single(i => i.Name == "Piano");
 
-            var createPupil = new CreatePupil("Pupil Name", 21M, DateTime.Today, 14, piano.Id, "Contact Name", "Contact Email", "Contact Phone Number");
-            response = await _client.PostAsJsonAsync<CreatePupil>(PupilsUri, createPupil);
+            return piano;
+        }
+
+        private async Task GetPupilAndValidate(PupilResponseDto pupil)
+        {
+            var response = await _client.GetAsync($"{PupilsUri}/{pupil.Id}");
+            response.StatusCode.Should().Be(StatusCodes.Status200OK);
+
+            var retrievedPupil = await response.Content.ReadAsAsync<PupilResponseDto>();
+            
+            retrievedPupil.Should().Be(pupil);
+        }
+
+        private async Task<PupilResponseDto> CreatePupilAndValidate(CreatePupil createPupil)
+        {
+            var response = await _client.PostAsJsonAsync<CreatePupil>(PupilsUri, createPupil);
             response.StatusCode.Should().Be(StatusCodes.Status201Created);
 
             var pupilResponse = await response.Content.ReadAsAsync<PupilResponseDto>();
@@ -43,10 +72,14 @@ namespace MusicTutor.IntegrationTests
             pupilResponse.ContactEmail.Should().Be(createPupil.ContactEmail);
             pupilResponse.ContactPhoneNumber.Should().Be(createPupil.ContactPhoneNumber);
 
-            var updatePupil = new UpdatePupil(pupilResponse.Id, "New Name", pupilResponse.LessonRate + 1, pupilResponse.StartDate.AddDays(-1), 7, "New Contact Name", "New Contact Email", "New PhoneNo" );
-            response = await _client.PutAsJsonAsync<UpdatePupil>($"{PupilsUri}/{updatePupil.Id}", updatePupil);
+            return pupilResponse;
+        }
+
+        private async Task<PupilResponseDto> UpdatePupilAndValidate(UpdatePupil updatePupil)
+        {
+            var response = await _client.PutAsJsonAsync<UpdatePupil>($"{PupilsUri}/{updatePupil.Id}", updatePupil);
             response.StatusCode.Should().Be(StatusCodes.Status200OK);
-            pupilResponse = await response.Content.ReadAsAsync<PupilResponseDto>();
+            var pupilResponse = await response.Content.ReadAsAsync<PupilResponseDto>();
             pupilResponse.Name.Should().Be(updatePupil.Name);
             pupilResponse.LessonRate.Should().Be(updatePupil.LessonRate);
             pupilResponse.FrequencyInDays.Should().Be(updatePupil.FrequencyInDays);
@@ -55,7 +88,12 @@ namespace MusicTutor.IntegrationTests
             pupilResponse.ContactEmail.Should().Be(updatePupil.ContactEmail);
             pupilResponse.ContactPhoneNumber.Should().Be(updatePupil.ContactPhoneNumber);
 
-            response = await _client.DeleteAsync($"{PupilsUri}/{pupilResponse.Id}");
+            return pupilResponse;
+        }
+
+        private async Task DeletePupilAndValidate(Guid pupilId)
+        {
+            var response = await _client.DeleteAsync($"{PupilsUri}/{pupilId}");
             response.StatusCode.Should().Be(StatusCodes.Status204NoContent);
         }
     }
