c805744f258e9fa995b21fdd05b828a8690e4aeb
Matt Hooper <matt.j.hooper@googlemail.com>
2021-03-29 22:03:04 +0100

    Added Bogus to create fake data

--- MusicTutor.IntegrationTests/PupilsControllerTests.cs
+++ MusicTutor.IntegrationTests/PupilsControllerTests.cs
@@ -10,39 +10,58 @@ using System.Linq;
 using MusicTutor.Api.Commands.Pupils;
 using MusicTutor.Api.Contracts.Pupils;
 using Xunit.Abstractions;
+using Bogus;
 
 namespace MusicTutor.IntegrationTests
 {
-    public class PupilsControllerTests: IntegrationTest
-    {        
-        
-        public PupilsControllerTests(ApiWebApplicationFactory fixture, ITestOutputHelper testOutputHelper) : base(fixture, testOutputHelper){ }
+    public class PupilsControllerTests : IntegrationTest
+    {
+
+        public PupilsControllerTests(ApiWebApplicationFactory fixture, ITestOutputHelper testOutputHelper) : base(fixture, testOutputHelper) { }
         [Fact]
         public async Task PupilCanBeCreatedUpdatedAndDeleted()
         {
-            var piano = await GetPiano();
+            var piano = await GetInstrument("Piano");
 
             var createPupilRequest = new CreatePupil("Pupil Name", 21M, DateTime.Today, 14, piano.Id, "Contact Name", "Contact Email", "Contact Phone Number");
             var pupilResponse = await CreatePupilAndValidate(createPupilRequest);
 
             await GetPupilAndValidate(pupilResponse);
 
-            var updatePupilRequest = new UpdatePupil(pupilResponse.Id, "New Name", pupilResponse.LessonRate + 1, pupilResponse.StartDate.AddDays(-1), 7, "New Contact Name", "New Contact Email", "New PhoneNo" );
+            var updatePupilRequest = new UpdatePupil(pupilResponse.Id, "New Name", pupilResponse.LessonRate + 1, pupilResponse.StartDate.AddDays(-1), 7, "New Contact Name", "New Contact Email", "New PhoneNo");
             pupilResponse = await UpdatePupilAndValidate(updatePupilRequest);
 
             await DeletePupilAndValidate(pupilResponse.Id);
         }
 
-        private async Task<InstrumentResponseDto> GetPiano()
+        [Fact(Skip = "Don't create too much data")]
+        public async Task CanCreateLotsOfPupils()
         {
-            _output.WriteLine($"Get piano");
+            var instrumentNames = new[] { "Drums", "Flute", "Guitar", "Piano" };
+
+            var faker = new Faker("en_GB");
+
+            for (int x = 0; x < 100; x++)
+            {
+                var instrument = await GetInstrument(faker.PickRandom(instrumentNames));
+                var contact = new Bogus.Person();
+                var createPupilRequest = new CreatePupil(faker.Name.FullName(), faker.Finance.Amount(12, 20, 2), faker.Date.Recent(7).Date, faker.Random.ArrayElement<int>(new int[] { 7, 14 }), instrument.Id, contact.FullName, contact.Email, contact.Phone);
+                var pupilResponse = await CreatePupilAndValidate(createPupilRequest);
+
+                await GetPupilAndValidate(pupilResponse);
+            }
+        }
+
+        private async Task<InstrumentResponseDto> GetInstrument(string instrumentName)
+        {
+            _output.WriteLine($"Get {instrumentName}");
             var response = await _client.GetAsync(InstrumentsUri);
             response.StatusCode.Should().Be(StatusCodes.Status200OK);
 
             var content = await response.Content.ReadAsAsync<List<InstrumentResponseDto>>();
-            var piano = content.Single(i => i.Name == "Piano");
+            var instrument = content.Single(i => i.Name == instrumentName);
 
-            return piano;
+            return instrument;
         }
 
         private async Task GetPupilAndValidate(PupilResponseDto pupil)
@@ -52,7 +71,7 @@ namespace MusicTutor.IntegrationTests
             response.StatusCode.Should().Be(StatusCodes.Status200OK);
 
             var retrievedPupil = await response.Content.ReadAsAsync<PupilResponseDto>();
-            
+
             retrievedPupil.Should().Be(pupil);
         }
 
@@ -68,7 +87,7 @@ namespace MusicTutor.IntegrationTests
             pupilResponse.LessonRate.Should().Be(createPupil.LessonRate);
             pupilResponse.FrequencyInDays.Should().Be(createPupil.FrequencyInDays);
             pupilResponse.StartDate.Should().Be(createPupil.StartDate);
-            pupilResponse.ContactName.Should().Be(createPupil.ContactName);            
+            pupilResponse.ContactName.Should().Be(createPupil.ContactName);
             pupilResponse.ContactEmail.Should().Be(createPupil.ContactEmail);
             pupilResponse.ContactPhoneNumber.Should().Be(createPupil.ContactPhoneNumber);
 
@@ -85,7 +104,7 @@ namespace MusicTutor.IntegrationTests
             pupilResponse.LessonRate.Should().Be(updatePupil.LessonRate);
             pupilResponse.FrequencyInDays.Should().Be(updatePupil.FrequencyInDays);
             pupilResponse.StartDate.Should().Be(updatePupil.StartDate);
-            pupilResponse.ContactName.Should().Be(updatePupil.ContactName);            
+            pupilResponse.ContactName.Should().Be(updatePupil.ContactName);
             pupilResponse.ContactEmail.Should().Be(updatePupil.ContactEmail);
             pupilResponse.ContactPhoneNumber.Should().Be(updatePupil.ContactPhoneNumber);
 
